e3da63208107ee434d3f8c9147a0487b2b5cbf7b
CondeArmand <neoarmandosousa1010@hotmail.com>
2023-10-17 17:00:51 -0300

    Adicionada função para deletar um jogo do player

--- src/db/conn.js
+++ src/db/conn.js
@@ -7,13 +7,13 @@ async function main() {
     try {
         mongoose.set("strictQuery", true)
 
-        // await mongoose.connect(
-        //     `mongodb+srv://condearmand:${mongoPassword}@cluster0.3nrm5es.mongodb.net/?retryWrites=true&w=majority`
-        // )
-
         await mongoose.connect(
-            `mongodb://conde:montepicos@localhost:27017/tests?authSource=admin`
+            `mongodb+srv://condearmand:${mongoPassword}@cluster0.3nrm5es.mongodb.net/?retryWrites=true&w=majority`
         )
+
+        // await mongoose.connect(
+        //     `mongodb://conde:montepicos@localhost:27017/tests?authSource=admin`
+        // )
 
         console.log("Conectado com o banco!")
     } catch (error) {
